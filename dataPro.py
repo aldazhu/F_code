30d4584c6865590f059bac992e80e344d87a44b8
wuming <hello>
2023-02-08 22:27:54 +0800

    feat: 添加画蜡烛图的方法

--- dataPro.py
+++ dataPro.py
@@ -2,6 +2,7 @@ import baostock as bs
 import pandas as pd
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 # download data from Internet
 def downloadData(stockCode:str,startDate:str,endDate:str,frequency:str="d"):
@@ -116,6 +117,14 @@ class dataPro():
         pass
 
     def readData(self,csvFilePath):
+        """Input hte path of the csv file ,return the data of csv in panda frame format
+
+        Args:
+            csvFilePath (str): the path of the csv
+
+        Returns:
+            panda frame : the stock data
+        """
         self.data = pd.read_csv(csvFilePath)
         return self.data
 
@@ -129,7 +138,7 @@ class dataPro():
         x = []
         y = (self.data['close'][dateIndex-1 + futureDays] - self.data['close'][dateIndex-1])/self.data['close'][dateIndex-1]
         for i in range(preDays):
-            x.insert(0,self.data['open'][dateIndex -1 - i])
+            x.insert(0,self.data['open'][dateIndex -1 - i])# 插入在最前面
             x.insert(0,self.data['close'][dateIndex -1 - i])
             x.insert(0,self.data['high'][dateIndex -1 - i])
             x.insert(0,self.data['low'][dateIndex -1 - i])
@@ -146,6 +155,101 @@ class dataPro():
             _x.insert(0,x)
             _y.insert(0,y)
         return np.array(_x),np.array(_y)
+
+class stockCluster():
+    def __init__(self,preDays=5,featureDays=5) -> None:
+        """_summary_
+
+        Args:
+            preDays (int, optional): 取今天前preDays的交易数据. Defaults to 5.
+            featureDays (int, optional): 取featureDays天后相对于今天的变化百分比. Defaults to 5.
+        """
+        pass
+        self.preDays_ = preDays
+        self.featureDays_ = featureDays 
+
+    def getShiftData(self,csvFilePath):
+        """对于每一个样本点取前preDays天的数据（开盘价，最高价，最低价，收盘价，交易股数，换手率）做特征，每一个样本都用第一天的数据转为百分比值，
+        例如：第一天的开盘价为10元，第二天的开盘价为10.1元，则第二天的开盘价变为，(10.1 - 10) / 10 = 0.01,第一天的开盘价变为(10-10)/10 = 0,
+        self.featureDays_后close相对于今天的变化百分比
+        返回类型：X=[    [day1_open,day1_high, day1_low,...,day2_open,day2_high,day2_low,...,day5_open,...],#样本1
+                        [day2_open,day2_high, day2_low,...,day3_open,day4_high,day5_low,...,day6_open,...],#样本2
+                        ...
+                    ]
+                Y=[
+                    featuteDays_change,#样本1的标签
+                    featuteDays+1_change,#样本2的标签
+                    ...
+                    ]
+        
+        Args:
+            csvFilePath (str) : 股票文件的路径
+           
+        """
+        data = pd.read_csv(csvFilePath)
+        day_num = len(data["open"])
+
+        X = []
+        Y = []
+        if day_num < self.preDays_ + self.featureDays_:
+            return X,Y
+
+        for i in range(day_num - self.featureDays_ - self.preDays_):
+            # 提取某一特征self.preDays_天的数据，防止除以0 所以加一个很小的数“1e-9”(10的负九次方)
+            # 涉及到价格的都以第一天的开盘价为0基准
+            feat = lambda item: (data[item][i:i+self.preDays_] - data["open"][i]) / data["open"][i]  
+
+            #x_i self.preDays_天的交易数据，以第一天的数据作为基准，后面几天的都用相对于第一天的百分比
+
+            volumes = (data["volume"][i:i+self.preDays_] - data["volume"][i]) / (data["volume"][i] + 1e-9)
+            turns = (data["turn"][i:i+self.preDays_] - data["turn"][i]) / (data["turn"][i] + 1e-9)
+            x_i = [feat("open"),feat("high"),feat("low"),feat("close"),volumes,turns]
+            y_i = (data["close"][i+self.preDays_+self.featureDays_-1] - data["close"][i+self.preDays_-1] ) / data["close"][i+self.preDays_-1]
+
+            # x_x_array = [[day1_open,day1_high, day1_low,...],[day2_open,day2_high,day2_low,...],[day3_open,...]..]
+            x_i_array = np.array(x_i).T
+            # 把x_i_array变成单行的向量
+            item_num, days = x_i_array.shape[:2]
+            x_i_array = x_i_array.reshape(item_num*days)
+            y_i_array = np.array(y_i)
+            X.append(x_i_array)
+            Y.append(y_i_array)
+        return X,Y
+
+    def drawOneSample(self,sample,y):
+        """sample preDays天的（开盘价，最高价，最低价，收盘价，交易股数，换手率）组成的样本，
+        [day1_open,day1_high, day1_low,...,day2_open,day2_high,day2_low,...,day5_open,...]
+
+        Args:
+            sample (_type_): _description_
+            y (_type_): _description_
+        """
+        pass
+        dates = np.arange(self.preDays_)
+        # 每天的6项特征,open在第一位，每个6个数据是一个open价格
+        opens = np.array(sample[::6]) 
+        highs = np.array(sample[1::6])
+        lows = np.array(sample[2::6])
+        closes = np.array(sample[3::6])
+        volumes = np.array(sample[4::6])
+        turns = np.array(sample[5::6])
+
+        colors_bool = closes >= opens
+        colors = np.zeros(colors_bool.size, dtype="U5")
+        colors[:] = "blue"
+        colors[colors_bool] = "white"
+
+        edge_colors = np.zeros(colors_bool.size, dtype="U1")
+        edge_colors[:] = "b"
+        edge_colors[colors_bool] = "r"
+
+
+        plt.bar(dates, (closes - opens), 0.8, bottom=opens, color=colors,edgecolor=edge_colors, zorder=3)
+        plt.vlines(dates, lows, highs, color=edge_colors)
+        plt.show()
+
+
+    
 
 
 def downloadDataDemo():
@@ -165,6 +269,18 @@ def downloadHS300Demo():
     downloadHS300(saveFolder, startDate, endDate)
     
 
+def clusterDemo():
+    csv_file_path = r"./data/sh.600000.csv"
+
+    preDays = 5
+    featureDays = 5
+    tool = stockCluster(preDays,featureDays)
+    X,Y = tool.getShiftData(csv_file_path)
+    tool.drawOneSample(X[0],Y[0])
+
+
 if __name__ == "__main__":
-    downloadDataDemo()
+    pass
+    clusterDemo()
+    # downloadDataDemo()
     # downloadHS300Demo()
